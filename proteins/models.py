7a167dd6404c554e922d93438db58ef7c2a22ef4
Sam Corella <samcorella77@gmail.com>
2025-01-31 21:57:09 -0500

    Register Proteins app with admin page

--- proteins/models.py
+++ proteins/models.py
@@ -3,5 +3,8 @@ from django.db import models
 class Food(models.Model):
     name = models.CharField(max_length=255)
     source = models.CharField(max_length=255) # Plant or Meat
-
+    
     # protein and other nutrient info ...
+
+    def __str__(self):
+        return self.name
