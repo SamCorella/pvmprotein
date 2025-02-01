ecf5e654aa01e0dbacac1d73e6b7f698c4514c20
Sam Corella <samcorella77@gmail.com>
2025-02-01 10:50:26 -0500

    Add protein and cost fields to Food model

--- proteins/models.py
+++ proteins/models.py
@@ -3,8 +3,10 @@ from django.db import models
 class Food(models.Model):
     name = models.CharField(max_length=255)
     source = models.CharField(max_length=255) # Plant or Meat
+    protein = models.IntegerField(default=0) # Grams of protein
+    cost = models.DecimalField(max_digits=4, decimal_places=2, default=00.00)
     
-    # protein and other nutrient info ...
+    # Other nutrient info ...
 
     def __str__(self):
         return self.name
